9a36d507a62b83b6117c268c76a0f207ea4c6afc
AndrwYan <beyond1997110@gmail.com>
2022-03-20 17:56:48 +0800

    1.增加transfer的代码

---
+++ api/transfer.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	db "github.com/AndrewLoveMei/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//创建请求体
+type transferRequest struct {
+	FromAccountId int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountId   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,oneof=USD EUR CAD"`
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req transferRequest
+	//将请求中的数据绑定到相应的结构体之中
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	if !server.validAccount(ctx, req.FromAccountId, req.Currency) {
+		return
+	}
+
+	if !server.validAccount(ctx, req.ToAccountId, req.Currency) {
+		return
+	}
+
+	//货币currency需要将fromaccountid和toaccountid相比较
+	arg := db.TransferTxParams{
+		FromAccountId: req.FromAccountId,
+		ToAccountId:   req.ToAccountId,
+		Amount:        req.Amount,
+	}
+
+	result, err := server.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
+
+}
+
+//将FromAccountId,ToAccountId对应的参数做校验对货币做校验的函数
+func (server *Server) validAccount(ctx *gin.Context, accountId int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountId)
+
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+	if account.Currency != currency {
+
+		//自定义异常
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+
+	}
+
+	return true
+}
