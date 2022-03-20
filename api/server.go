f92b0419b436b6889314a45939a63332925cf603
AndrwYan <beyond1997110@gmail.com>
2022-03-20 18:00:57 +0800

    1.增加transfer的路由

--- api/server.go
+++ api/server.go
@@ -17,6 +17,7 @@ func NewServer(store *db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
+	router.POST("/transfer", server.createTransfer)
 
 	//add router to router
 	server.router = router
