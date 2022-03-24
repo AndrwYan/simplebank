1e58a1724210ab7de73f0c9e1432a3ba6d3488b8
AndrwYan <beyond1997110@gmail.com>
2022-03-24 21:31:18 +0800

    1.对出现外键约束的报错返回403状态码

--- api/account.go
+++ api/account.go
@@ -35,6 +35,10 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok { //因为外键约束而增加的条件
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation": //外键约束和唯一性约束
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+			}
 			log.Println(pqErr.Code.Name())
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
