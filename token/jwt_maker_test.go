8b02ab65d39212cd4ec89931b74d7846621f7e92
AndrwYan <beyond1997110@gmail.com>
2022-03-26 16:46:02 +0800

    1.关于jwt的测试实现

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,34 @@
+package token
+
+import (
+	"github.com/AndrewLoveMei/simplebank/db/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+	require.NotEmpty(t, maker)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issueAt := time.Now()
+	expiredAt := issueAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	//验证token
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issueAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Minute)
+}
